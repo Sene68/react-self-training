db53f6012506c4213f087571cb91237b88b5c1f0
Sene68 <senefull68@gmail.com>
2022-04-02 10:56:12 +0900

    useState - add task

--- todo-list/todo-list-react/src/App.js
+++ todo-list/todo-list-react/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { nanoid } from "nanoid";
 import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 
-function addTask(name) {
-  alert(name);
-}
+
 
 function App(props) {
-  const taskList = props.tasks.map(task => (
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  const taskList = tasks.map(task => (
     <Todo
       id={task.id}
       name={task.name}
