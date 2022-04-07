d05b1618d7f61f3cdbd10e6c57b4034c65049693
Sene68 <senefull68@gmail.com>
2022-04-07 22:38:01 +0900

    training1 - conditional rendering

--- training-1/src/App.js
+++ training-1/src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
-import './App.css';
 
 
 function App(props) {
-  const subject = props.subject;
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24, 
-    padding: '1rem' 
-  }
+
+
 
   return (
     <Wrapper>
-      <Hello name="react" color="red"/>
-      <div style={style}>{subject}</div>
-      <div className="gray-box"></div>
+      <Hello name="react" color="red" isSpecial/>
+      <Hello color="pink" />
     </Wrapper>
   );
 }
