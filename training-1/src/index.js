24b0d7e8152a5473c2b3124e88bf046bc07056b8
Sene68 <senefull68@gmail.com>
2022-04-19 20:51:52 +0900

    training1 - useReducer-2

--- training-1/src/index.js
+++ training-1/src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as reportWebVitals from './reportWebVitals';
-import Counter from './Counter';
 
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
