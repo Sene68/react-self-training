f548024e3747b61cfe5ecd93e4c2fa4068cc5507
Sene68 <senefull68@gmail.com>
2022-05-03 22:50:13 +0900

    training4 - Link 연결하기

--- training-4/router-tutorial/src/App.js
+++ training-4/router-tutorial/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
